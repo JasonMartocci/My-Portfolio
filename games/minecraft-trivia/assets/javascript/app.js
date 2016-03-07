b605eed94ad70a09a14b52bcd3e1a16aa5c72812
Jason Martocci <Jason Martocci>
2016-03-07 11:07:55 -0500

    cleaned up the code for the trivia game

--- games/minecraft-trivia/assets/javascript/app.js
+++ games/minecraft-trivia/assets/javascript/app.js
@@ -49,35 +49,42 @@ var stopwatch = {
     }
     return minutes + ":" + seconds;
   }
-
 };
 
+// Time is up function that i never got to work properly. The issue I am having is displaying the next question. 
 function timeUp(){
   unanswered++;
-  // $("#result").empty().append("<button class='incorrectAns'>TIME IS UP</button><br>");
-  // setTimeout(threeSeconds, 3000);
+  // $("#result").append("<button class='incorrectAns'>TIME IS UP!</button><br>");
+  //   setTimeout(threeSeconds, 3000);
+  //   function threeSeconds() {
+  //     questionTwo();
+  //     stopwatch.reset();
+  //   };
 }
 
-// Trivia questions
+// Function to display if the answer was right or wrong.
+var rightWrong = function(){
+  $("button").click(function(){
+      var ans = $(this);
+      if(ans.val() === "correct"){
+        correct++;
+        $("#result").append("<button class='correctAns'>CORRECT</button><br>");
+      }else if(ans.val() === "incorrect"){
+        incorrect++;
+        $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
+      }else{
+        unanswered++;
+      }
+  }); 
+}
+
+// Trivia questions start
 function questionOne(){
   $("#question").empty().append("<h3>There are many blocks in 'Minecraft', but only two are affected by gravity. Which two are affected?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>Sand and Bedrock</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>Bedrock and Gravel</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='correct'>Gravel and Sand</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>Gravel and Dirt</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Sand and Bedrock</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Bedrock and Gravel</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Gravel and Sand</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Gravel and Dirt</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -87,28 +94,15 @@ function questionOne(){
             stopwatch.reset();
         }
     });
-
 }
 
 function questionTwo(){
+  $("#result").empty().append("");
   $("#question").empty().append("<h3>What is the number of items that add up to a stack?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>999</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='correct'>64</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>23</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>44</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>999</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>64</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>23</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>44</button><br>"); 
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -122,23 +116,10 @@ function questionTwo(){
 
 function questionThree(){
   $("#question").empty().append("<h3>What is the name of a block that might make up most of your built 'Minecraft' world?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>Earthstone</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>Lavastone</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>Redstone</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='correct'>Cobblestone</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Earthstone</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Lavastone</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Redstone</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Cobblestone</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -152,23 +133,10 @@ function questionThree(){
 
 function questionFour(){
   $("#question").empty().append("<h3>What does a zombie drop when killed in 'Minecraft' 1.4.5?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>Cobblestone</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>Gold ingot</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='correct'>Rotten flesh</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>Meat</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Cobblestone</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Gold ingot</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Rotten flesh</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Meat</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -182,23 +150,10 @@ function questionFour(){
 
 function questionFive(){
   $("#question").empty().append("<h3>What do you use to take wool from a sheep?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>Sword</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='correct'>Shears</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>Lava</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>Water</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Sword</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Shears</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Lava</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Water</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -212,23 +167,10 @@ function questionFive(){
 
 function questionSix(){
   $("#question").empty().append("<h3>Who is the 'feared player' in 'Minecraft'?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>Jeb</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>Skydoesminecraft</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='correct'>Herobrine</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>Notch</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Jeb</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Skydoesminecraft</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Herobrine</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Notch</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -242,23 +184,10 @@ function questionSix(){
 
 function questionSeven(){
   $("#question").empty().append("<h3>How do you sneak (by default) in the PC version of 'Minecraft'?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='incorrect'>By Pressing H</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='correct'>By Pressing Shift</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>By Pressing S</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>By Pressing 3</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>By Pressing H</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>By Pressing Shift</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>By Pressing S</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>By Pressing 3</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -272,23 +201,10 @@ function questionSeven(){
 
 function questionEight(){
   $("#question").empty().append("<h3>What is the name of the 'Minecraft' Hell?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='correct'>The Nether</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>The Hether</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>The Aether</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>The Cether</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>The Nether</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Hether</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Aether</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Cether</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -302,23 +218,10 @@ function questionEight(){
 
 function questionNine(){
   $("#question").empty().append("<h3>Which is NOT a Nether mob?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='correct'>The Devil</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>The Blaze</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>The Ghast</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>The Zombie Pigmen</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>The Devil</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Blaze</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Ghast</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>The Zombie Pigmen</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -332,23 +235,10 @@ function questionNine(){
 
 function questionTen(){
   $("#question").empty().append("<h3>How many colors of wool are there in 'Minecraft'?</h3>");
-  $("#answerOne").empty().append("<button class='submit' value='correct'>Sixteen</button><br>");
-  $("#answerTwo").empty().append("<button class='submit' value='incorrect'>Eighteen</button><br>");
-  $("#answerThree").empty().append("<button class='submit' value='incorrect'>Ten</button><br>");
-  $("#answerFour").empty().append("<button class='submit' value='incorrect'>Seventeen</button><br>");
-
-    $("button").click(function(){
-        var ans = $(this);
-        if(ans.val() === "correct"){
-          correct++;
-          $("#result").append("<button class='correctAns'>CORRECT</button><br>");
-        }else if(ans.val() === "incorrect"){
-          incorrect++;
-          $("#result").append("<button class='incorrectAns'>INCORRECT</button><br>");
-        }else{
-          unanswered++;
-        }
-    }); 
+  $("#answerOne").empty().append("<button onclick='rightWrong()' class='submit' value='correct'>Sixteen</button><br>");
+  $("#answerTwo").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Eighteen</button><br>");
+  $("#answerThree").empty().append("<button onclick='rightWrong()' onclick='rightWrong()' class='submit' value='incorrect'>Ten</button><br>");
+  $("#answerFour").empty().append("<button onclick='rightWrong()' class='submit' value='incorrect'>Seventeen</button><br>");
 
     $(".submit").click(function(){
       setTimeout(threeSeconds, 3000);
@@ -359,20 +249,16 @@ function questionTen(){
         }
     });
 }
+// Trivia questions end
 
+// Results page
 function results(){
-
     $("#question").empty().append("<h1>All done, here is how you did!</h1><br><br><h3>Correct Answers: " + correct + "</h3><br><br>" + "<h3>Incorrect Answers: " + incorrect + "</h3><br><br>" + "<h3>Unanswered: " + unanswered + "</h3>");
     $("#answerOne").empty().append("<button id='start' onclick='stopwatch.start()'>Start New Game</button>");
     $("#answerTwo").empty().append("");
     $("#answerThree").empty().append("");
     $("#answerFour").empty().append("");
-
   }
-
-
-
-
 
 
 // Only one question will be shown at a time.
